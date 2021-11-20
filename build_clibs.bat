9dbd9a521321afb333f73ff294cecc038a652a48
zhad3 <dragoonzombie@gmail.com>
2021-11-20 22:02:09 +0100

    Fix windows build process

--- build_clibs.bat
+++ build_clibs.bat
@@ -6,9 +6,15 @@ set ARCH=%DUB_ARCH%
 set BUILD_TYPE=%DUB_BUILD_TYPE%
 set BUILD_CONFIG="Release Library"
 
+echo PACKAGE_DIR=%PACKAGE_DIR%
+echo ARCH=%ARCH%
+echo BUILD_TYPE=%BUILD_TYPE%
+
 IF "%BUILD_TYPE%"=="debug" (set BUILD_CONFIG="Debug Library")
 
 set DEST_DIR=%PACKAGE_DIR%\c\build\%ARCH%-%BUILD_TYPE%
+
+echo DEST_DIR=%DEST_DIR%
 
 IF NOT EXIST "%DEST_DIR%" (
     mkdir "%DEST_DIR%"
@@ -23,9 +29,11 @@ IF NOT DEFINED VSINSTALLDIR (
     exit /b 1
 )
 
-cd "%PACKAGE_DIR%\c\lpng1637\projects\vstudio"
+cd /d "%PACKAGE_DIR%\c\lpng1637\projects\vstudio"
+echo CD=%CD%
 
 set OUT_DIR="%ARCH%-%BUILD_TYPE%"
+echo OUT_DIR=%OUT_DIR%
 
 msbuild.exe vstudio.sln /p:OutDir=%OUT_DIR% /t:libpng /p:Configuration=%BUILD_CONFIG% /p:WarningLevel=Level1 /p:TreatWarningAsError="false" /p:IntermediateOutputPath=%OUT_DIR%\
 
@@ -35,4 +43,4 @@ IF "%BUILD_TYPE%"=="debug" (move "libpng\%OUT_DIR:"=%\libpng.pdb" "%DEST_DIR%\li
 REM Clean the project. This step is neccessary for following builds, otherwise build errors will be thrown
 msbuild.exe vstudio.sln /t:clean /p:Configuration=%BUILD_CONFIG% /p:IntermediateOutputPath=%OUT_DIR%\
 
-cd %OLD_CD%
+cd /d %OLD_CD%
